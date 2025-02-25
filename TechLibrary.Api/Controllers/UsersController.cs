2676d0f28f5ec678f58dace076977531b47e3aa6
gp208 <gustavocprieto@gmail.com>
2025-02-24 22:51:31 -0300

    controller

---
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechLibrary.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register()
+    {
+        return Created();
+    }
+}
