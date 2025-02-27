613700e26a5358cd7b46dd3c25b06547b98052e6
gp208 <gustavocprieto@gmail.com>
2025-02-27 11:34:31 -0300

    login

---
+++ TechLibrary.Exception/InvalidLoginException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace TechLibrary.Exception;
+
+public class InvalidLoginException : TechLibraryException
+{
+    public override List<string> GetErrorMessages() => ["Email e/ou senha inválidos"];
+    public override HttpStatusCode GetStatusCode() => HttpStatusCode.Unauthorized;
+}
