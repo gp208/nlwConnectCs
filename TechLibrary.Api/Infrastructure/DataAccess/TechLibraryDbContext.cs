31170778e8e0ad7057ad88e2ff12724aab562330
gp208 <gustavocprieto@gmail.com>
2025-02-28 11:56:53 -0300

    books

--- TechLibrary.Api/Infrastructure/DataAccess/TechLibraryDbContext.cs
+++ TechLibrary.Api/Infrastructure/DataAccess/TechLibraryDbContext.cs
@@ -5,7 +5,8 @@ namespace TechLibrary.Api.Infrastructure.DataAccess;
 
 public class TechLibraryDbContext : DbContext
 {
-    public DbSet<User> Users { get; set; } // 'Users'= table name
+    public DbSet<User> Users { get; set; } // table name
+    public DbSet<Book> Books { get; set; } // table name
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {// db file path
         optionsBuilder.UseSqlite("Data Source=");
---
+++ TechLibrary.Api/UseCases/Books/Filter/FilterBookUseCase.cs
@@ -0,0 +1,23 @@
+﻿using TechLibrary.Api.Infrastructure.DataAccess;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+
+namespace TechLibrary.Api.UseCases.Books.Filter;
+
+public class FilterBookUseCase
+{
+    public ResponseBooksJson Execute(RequestFilterBooksJson request)
+    {
+        var dbContext = new TechLibraryDbContext();
+        var books = dbContext.Books.ToList(); // table name
+        return new ResponseBooksJson
+        {
+            Books = books.Select(book => new ResponseBookJson
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Author = book.Author
+            }).ToList()
+        };
+    }
+}
